6de5cada76538696a6deeed3b4f8c85d962857c4
JamepDev <JamepGames@gmail.com>
2024-12-06 11:49:06 -0600

    const

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -7,9 +7,10 @@ use esp_hal::{
 };
 use log::info;
 
+const SET_POINT: f32 = 2.5;
+
 #[entry]
 fn main() -> ! {
-    let set_point: f32 = 2.5;
     esp_println::logger::init_logger_from_env();
     let peripherals = esp_hal::init({
         let mut config: esp_hal::Config = esp_hal::Config::default();
@@ -29,14 +30,12 @@ fn main() -> ! {
     let delay = Delay::new();
     
     loop {
-        let output: f32;
         let pin_value: u16 = nb::block!(adc1.read_oneshot(&mut pin)).unwrap();
         let pin_value: f32 = ((pin_value as f32) * 3.3)/4095.0;
+        let output: f32 = pin_value - (pin_value-SET_POINT);
 
         info!("Input: {}", pin_value);
-        info!("Error: {}", (pin_value-set_point));
-
-        output = pin_value - (pin_value-set_point);
+        info!("Error: {}", (pin_value-SET_POINT));
         info!("Salida: {}", output);
         delay.delay(500.millis());
     }
